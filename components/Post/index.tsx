31d43fcf6404e34adb5df68086d6c0bedfa7cd05
Ivan Kashin <barbos1701@gmail.com>
2022-04-17 23:02:59 +0300

    Добавляет форму добавления комментария и оставления комментов

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -1,11 +1,19 @@
-import { Card, Divider } from "antd";
+import {
+  Button,
+  Card,
+  Comment as CommentComponent,
+  Divider,
+  Form,
+  Input,
+} from "antd";
 import Image from "next/image";
-import React, { FC } from "react";
+import React, { ChangeEventHandler, FC, useMemo, useState } from "react";
 import { useWindowSize } from "react-use";
 import { CommentContent } from "..";
 import { Comment } from "../../types/comment";
 import { Post } from "../../types/post";
 
+const { TextArea } = Input;
 interface PostProps {
   data: { post: Post; comments: Comment[] };
 }
@@ -14,7 +22,44 @@ const Post: FC<PostProps> = (props) => {
   const { post, comments } = props.data;
   const { url, title } = post;
 
+  const [userComments, setUserComments] = useState<Comment[]>([]);
+  const [commentText, setCommentText] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
   const { width } = useWindowSize();
+
+  const onCommentChange = (evt: any) => {
+    setCommentText(evt.target.value);
+  };
+
+  const onSubmit = () => {
+    const newId =
+      [...comments, ...userComments].reduce(
+        (maxId, comment) => (comment.id > maxId ? comment.id : maxId),
+        0
+      ) + 1;
+    setSubmitting(true);
+    setUserComments((prev) => [
+      ...prev,
+      {
+        postId: post.id,
+        id: newId,
+        name: "Вы",
+        email: "user@example.com",
+        body: commentText.trim(),
+      },
+    ]);
+
+    setTimeout(() => {
+      setSubmitting(false);
+      setCommentText("");
+    }, 1000);
+  };
+
+  const totalComments = useMemo(
+    () => [...comments, ...userComments],
+    [comments, userComments]
+  );
 
   return (
     <>
@@ -27,9 +72,34 @@ const Post: FC<PostProps> = (props) => {
 
       <Divider>Комментарии</Divider>
 
-      {comments.map((comment) => (
+      {totalComments.map((comment) => (
         <CommentContent key={comment.id} comment={comment} />
       ))}
+
+      <Divider>Оставьте свой комментарий</Divider>
+
+      <Card bordered={false}>
+        <CommentComponent
+          content={
+            <Form>
+              <Form.Item>
+                <TextArea value={commentText} onChange={onCommentChange} />
+              </Form.Item>
+              <Form.Item>
+                <Button
+                  htmlType="submit"
+                  loading={submitting}
+                  disabled={commentText.trim().length === 0}
+                  onClick={onSubmit}
+                  type="primary"
+                >
+                  Отправить
+                </Button>
+              </Form.Item>
+            </Form>
+          }
+        />
+      </Card>
     </>
   );
 };
